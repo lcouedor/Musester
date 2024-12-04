873877a9e7e823eef4beb64885407e3d28f738a5
lcouedor <leo.couedor@gmail.com>
2024-12-04 10:14:13 +0100

    rename

--- V3/backend/app.py
+++ V3/backend/app.py
@@ -8,4 +8,4 @@ def home():
     return jsonify({"message": "Welcome to the Flask Music API"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
