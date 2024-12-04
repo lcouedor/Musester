aa9fd14397cce673c5d04e5b052159ca97fc7032
lcouedor <leo.couedor@gmail.com>
2024-12-04 10:31:15 +0100

    test request

--- V3/frontend/src/main.tsx
+++ V3/frontend/src/main.tsx
@@ -14,8 +14,13 @@ async function test(){
   //Faire une requête distante avec axios
   let res = await axios.get('https://api.github.com/users/github')
   console.log(res.data)
+
+  let res2 = await axios.get('https://musester-back.onrender.com')
+  console.log(res2.data)
 }
 
 test()
+
+
 
 
